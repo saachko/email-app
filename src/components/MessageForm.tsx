a5358e407f0dda45a7d105e2f8a15f81a40e5ee1
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-01-29 00:38:05 +0300

    feat: add MessageForm

--- src/components/MessageForm.tsx
+++ src/components/MessageForm.tsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
 
 function MessageForm() {
-  return <div>MessageForm</div>;
+  return (
+    <Form>
+      <Form.Group className="mb-3 d-flex" controlId="exampleForm.ControlInput1">
+        <Form.Label column sm="3" className="me-2">
+          Recipient
+        </Form.Label>
+        <Form.Control type="text" placeholder="name@example.com" autoFocus />
+      </Form.Group>
+      <Form.Group className="mb-4 d-flex" controlId="exampleForm.ControlInput1">
+        <Form.Label column sm="3" className="me-2">
+          Subject
+        </Form.Label>
+        <Form.Control type="text" placeholder="(No subject)" autoFocus />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Control as="textarea" rows={3} placeholder="Your message..." />
+      </Form.Group>
+      <Button variant="primary" type="submit" className="w-100 mt-2">
+        Send message
+      </Button>
+    </Form>
+  );
 }
 
 export default MessageForm;
--- src/pages/MailPage.tsx
+++ src/pages/MailPage.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
 import Header from '../components/Header';
 import Mailbox from '../components/Mailbox';
 import MessageForm from '../components/MessageForm';
+import { getUsers } from '../utils/api';
 import { User } from '../utils/interfaces';
 import SetState from '../utils/types';
 
@@ -14,6 +15,18 @@ interface MailPageProps {
 }
 
 function MailPage({ isLoggedIn, setLoggedIn, currentUser }: MailPageProps) {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const currentUserId = localStorage.getItem('currentUserId');
+    (async () => {
+      if (currentUserId) {
+        const data = await getUsers();
+        setUsers(data);
+      }
+    })();
+  }, [currentUser]);
+
   if (!isLoggedIn) {
     return <Navigate to="/" />;
   }
@@ -23,7 +36,7 @@ function MailPage({ isLoggedIn, setLoggedIn, currentUser }: MailPageProps) {
         setLoggedIn={setLoggedIn}
         username={currentUser ? currentUser.username : ''}
       />
-      <div>
+      <div className="d-flex flex-column flex-md-row justify-content-between gap-4">
         <MessageForm />
         <Mailbox />
       </div>
