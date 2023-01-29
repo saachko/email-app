9e5c29cccd8759dda64347662ec2aae6b4af3bf7
Anastasiya Sachko <baron-nastya2009@yandex.ru>
2023-01-29 22:15:55 +0300

    fix: change backend link

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,4 @@
-const baseUrl = 'https://email-backend-hut3.onrender.com/';
+const baseUrl = 'https://email-backend2.onrender.com/';
 const messages = `${baseUrl}messages`;
 const send = `${messages}/send`;
 const receive = `${baseUrl}messages/receive`;
